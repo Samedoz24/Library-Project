4873d46100f71e795872375728d8078eec2ba460
Samed Oz <sametoz706@gmail.com>
2025-08-08 13:33:05 +0300

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Library-Project/", // repo adın burası
   plugins: [react()],
-  base: "/Libarary Projec",
 });
