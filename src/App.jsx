d48e463b75de142e16c6e0f80120512d056425cc
Samed Oz <sametoz706@gmail.com>
2025-08-09 18:58:41 +0300

    App commit edildi

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Navbar from "./components/Navbar";
 import SiteContext from "./context/SiteContext";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <div></div>
       <Navbar />
       <SiteContext />
     </>
