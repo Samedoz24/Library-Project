4a42b486c2f8d645449d2a5cfcb0c512d93cef95
Samed Oz <sametoz706@gmail.com>
2025-08-09 21:43:38 +0300

    main.jsx is changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+const basename = "/Library-Project";
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter
+    basename={basename}
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
     <App />
   </BrowserRouter>
 );
