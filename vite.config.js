cf47d4ad183b9c6f0888408a77f48f7f2c89080b
Samed Oz <sametoz706@gmail.com>
2025-08-12 15:24:53 +0300

    Herşey halloldu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Library-Project/", // repo adın burası
   plugins: [react()],
+  base: "./", // bu ekleme önemli
 });
