2116f64ace9e8de9237561c0028c33e9b7afa40a
Samed Oz <sametoz706@gmail.com>
2025-08-12 15:21:22 +0300

    Hatalar düzeltildi

--- src/context/SiteContext.jsx
+++ src/context/SiteContext.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Books from "../pages/Books";
 import Note from "../pages/Note";
-import TodoList from "../pages/TodoList";
+import ToDoList from "../pages/ToDoList";
 import Pomodoro from "../pages/Pomodoro";
 
 function SiteContext() {
@@ -13,7 +13,7 @@ function SiteContext() {
         <Route path="/" element={<Home />} />
         <Route path="/Books" element={<Books />} />
         <Route path="/note" element={<Note />} />
-        <Route path="/ToDoList" element={<TodoList />} />
+        <Route path="/ToDoList" element={<ToDoList />} />
         <Route path="/Pomodoro" element={<Pomodoro />} />
       </Routes>
     </>
